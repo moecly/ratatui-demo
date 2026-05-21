4bc3d75b6c3f46d87f63826daf37b50f64258572
moecly <1349269087ABC@gmail.com>
2026-05-21 10:12:42 +0800

    feat: 添加on_tick

--- src/app.rs
+++ src/app.rs
@@ -9,10 +9,28 @@ impl<'a> TabsState<'a> {
     }
 }
 
+struct Signal<S: Iterator> {
+    source: S,
+    points: Vec<S::Item>,
+    tick_rate: usize,
+}
+
+impl<S> Signal<S>
+where
+    S: Iterator,
+{
+    fn on_tick(&mut self) {
+        self.points.drain(0..self.tick_rate);
+        self.points
+            .extend(self.source.by_ref().take(self.tick_rate));
+    }
+}
+
 pub struct App<'a> {
     pub title: &'a str,
     pub tabs: TabsState<'a>,
     pub progress: f64,
+    progres_dir: bool,
 }
 
 impl<'a> App<'a> {
@@ -21,6 +39,24 @@ impl<'a> App<'a> {
             title: "ratatui-demo",
             tabs: TabsState::new(vec!["Tab0", "Tab1", "Tab2"]),
             progress: 0.0,
+            progres_dir: true,
+        }
+    }
+
+    pub fn on_tick(&mut self) {
+        match self.progres_dir {
+            true => {
+                self.progress = (self.progress + 0.001).min(1.0);
+                if self.progress >= 1.0 {
+                    self.progres_dir = false;
+                }
+            }
+            false => {
+                self.progress = (self.progress - 0.001).max(0.0);
+                if self.progress <= 0.0 {
+                    self.progres_dir = true;
+                }
+            }
         }
     }
 }
--- src/crossterm.rs
+++ src/crossterm.rs
@@ -1,4 +1,5 @@
 use std::time::Duration;
+use std::time::Instant;
 
 use color_eyre::eyre::Context;
 use color_eyre::eyre::Ok;
@@ -7,14 +8,24 @@ use crossterm::event;
 use crossterm::event::KeyCode;
 use ratatui::DefaultTerminal;
 
-use crate::ui;
 use crate::app::App;
+use crate::ui;
 
 pub fn run(terminal: &mut DefaultTerminal) -> Result<()> {
     let mut app = App::new();
+    let tick_rate = Duration::from_millis(1);
 
+    let mut last_tick = Instant::now();
     loop {
         terminal.draw(|frame| ui::render(frame, &mut app))?;
+
+        let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+        if !event::poll(timeout)? {
+            app.on_tick();
+            last_tick = Instant::now();
+            continue;;
+        }
+
         if should_quit()? {
             break;
         }
